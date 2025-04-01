32dcbcaca3c6a86586f053ecff7e817142dccb79
Sanghyup Lee <slee548@jhu.edu>
2025-04-01 18:11:59 -0400

    added featurednews

--- Tech-Lab-On-Campus/NewsFeed/frontend/src/components/FeaturedNews.tsx
+++ Tech-Lab-On-Campus/NewsFeed/frontend/src/components/FeaturedNews.tsx
@@ -2,30 +2,34 @@ import Link from "next/link";
 import { Article } from "@/utils/types";
 
 interface NewsCardProps {
-    article: Article;
+  article: Article;
 }
 
-
 function FeaturedNewsCard({ article }: NewsCardProps) {
-    // PART 1: Display a Featured News article
+  // PART 1: Display a Featured News article
 
-    // Using the info about the article passed in as a prop, show:
-    // 1. The featured article's title
-    // 2. The featured article's image
-    // 3. A portion of the selected article's body, truncated so that it fits nicely in the section
+  // Using the info about the article passed in as a prop, show:
+  // 1. The featured article's title
+  // 2. The featured article's image
+  // 3. A portion of the selected article's body, truncated so that it fits nicely in the section
 
-    // Once completing this part, you should be able to see the Featured News Article at the top of the page.
+  // Once completing this part, you should be able to see the Featured News Article at the top of the page.
 
-    // Hint: Some classes included in `globals.css` may help with styling.
+  // Hint: Some classes included in `globals.css` may help with styling.
 
-    return (
-        <>
-            <span className='instruction'>Part 1: Show Featured News</span>
-            <div className="featured-news-card">
-                {/* TODO: Remove the span above and implement "FeaturedNewsCard" */}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="featured-news-card">
+        <h1 className="featured-story-title">{article.title}</h1>
+        <div className="featured-news-img-div">
+          <img src={article.image_url} className="featured-news-img" />
+        </div>
+        <div className="featured-news-info">
+            <p className="featured-story-summary">{article.body}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default FeaturedNewsCard;
